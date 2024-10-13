a3266e5037f24a1b95536565cc2c2e890e304222
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-14 02:16:12 +0330

    refactor: modify a neme of a variable

--- Member.java
+++ Member.java
@@ -4,20 +4,20 @@ import java.util.*;
 public class Member {
     //First we define the variables of this class
     private final String memberId;
-    private final String Name;
+    private final String name;
     public ArrayList<Book> borrowedBooks;
     //Now we create its constructor
-    public Member(String memberId, String Name) {
+    public Member(String memberId, String name) {
         this.memberId = memberId;
-        this.Name = Name;
+        this.name = name;
         this.borrowedBooks = new ArrayList<>();
     }
     //We don't need 'getter and setter' because we don't want to change the private variables
     //Create borrowBook method and check the conditions
     public void borrowBook(Book book) {
-        if (borrowedBooks.contains(book))       System.out.println("That book is already borrowed");
-        else if (borrowedBooks.size() > 3)      System.out.println("Yuo can't more than 3 books");
-        else if (book.getIsAvailable() == false) System.out.println("That book is not available");
+        if (borrowedBooks.contains(book))       System.out.println("#####That book is already borrowed");
+        else if (borrowedBooks.size() > 3)      System.out.println("#####Yuo can't more than 3 books");
+        else if (book.getIsAvailable() == false) System.out.println("#####That book is not available");
         else {
             borrowedBooks.add(book);
             book.setIsAvailable(false);
@@ -26,7 +26,7 @@ public class Member {
     }
     //Create returnBook method
     public void returnBook(Book book) {
-        if (borrowedBooks.contains(book))   System.out.println("That book is not borrowed");
+        if (borrowedBooks.contains(book))   System.out.println("#####That book is not borrowed");
         else {
             borrowedBooks.remove(book);
             book.setIsAvailable(true);
@@ -36,7 +36,7 @@ public class Member {
     //Create printMemberInfo method
     public void printMemberInfo() {
         System.out.println("-----Member ID: " + memberId);
-        System.out.println("-----Name: " + Name);
+        System.out.println("-----Name: " + name);
         System.out.println("-----The borrowed books: ");
         for (Book book : borrowedBooks) {
             System.out.println(">>>Borrowed book number '" + borrowedBooks.indexOf(book)+1 + "' :");
