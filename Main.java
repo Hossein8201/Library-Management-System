21b9d6077436a96dd128eb2047a28caec4c9347d
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-15 23:43:18 +0330

    refactor: rewrite the steps of the Main class

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ public class Main {
         //Creating a new library
         Library library = new Library("Hossein Library");
         //Adding at least 5 books to the library
-        Book book1 = new Book("11233","math","Euler");
-        Book book2 = new Book("15255","physics","Stephen William Hawking");
-        Book book3 = new Book("12885","Material Engineering","Calister");
-        Book book4 = new Book("12345","Linear Algebra","Villiam");
-        Book book5 = new Book("22233","Circuits","Albert Tun");
+        Book book1 = new Book("00001","math","Euler");
+        Book book2 = new Book("00002","physics","Stephen William Hawking");
+        Book book3 = new Book("00003","Material Engineering","Calister");
+        Book book4 = new Book("00004","Linear Algebra","Villiam");
+        Book book5 = new Book("00005","Circuits","Albert Tun");
         //Adding books to the library's collection
         library.addBook(book1);
         library.addBook(book2);
@@ -18,22 +18,24 @@ public class Main {
         library.addBook(book4);
         library.addBook(book5);
         //Adding at least 2 members to the library
-        Member member1 = new Member("222222", "Ali");
-        Member member2 = new Member("333333", "Reza");
+        Member member1 = new Member("000001", "Ali");
+        Member member2 = new Member("000002", "Reza");
         //Adding members to the library's member collection
         library.addMember(member1);
         library.addMember(member2);
         //Simulating borrowing books by members
         library.borrowBook(member1, book1);     //Ali borrows book1
-        library.borrowBook(member1, book2);     //Ali borrows book2
-        library.borrowBook(member1, book3);     //Ali borrows book3
-        library.borrowBook(member1, book4);     //Ali borrows book4
+        library.borrowBook(member1, book2, book3, book4);     //Ali borrows book2 and book3 and book4
         library.borrowBook(member2, book4);     //Reza attempts to borrow book4 (already borrowed)
         library.borrowBook(member2, book5);     //Reza borrows book5
         //Simulating returning books
-        library.returnBook(member1, book1);     //Ali returns book1
+        library.returnBook(member1, book1, book3);     //Ali returns book1 and book3
         library.returnBook(member2, book2);     //Reza attempts to return book2 (don't borrowed by him)
         library.returnBook(member2, book5);     //Reza returns book5
+        //For two time:
+        library.borrowBook(member2, book1, book3);
+        library.borrowBook(member1, book5);
+        library.returnBook(member1, book2, book4);
         //Printing library information (all books and members)
         library.printLibraryInfo();
         //Adding more books and members as needed to check the condition of book id and member id.
