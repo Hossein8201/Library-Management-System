72e27266b972a84a04371eca832a752907e71f0b
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-10-15 23:10:43 +0330

    feat: add a possibility to borrow or return many books in a time

--- Library.java
+++ Library.java
@@ -4,50 +4,71 @@ import java.util.*;
 public class Library {
     //First we define the variables of this class
     private final String name;
-    public Collection<Book> books;
-    public Collection<Member> members;
+    private Collection<Book> books;
+    private Collection<Member> members;
     //Now we create its constructor
     public Library(String name) {
         this.name = name;
         this.books = new ArrayList<>();
         this.members = new ArrayList<>();
     }
-    //We don't need 'getter and setter' because we don't want to change the private variable
-    //Create methods of this class:
+    //getter methods
+    public String getName() {
+        return name;
+    }
+    public Collection<Book> getBooks() {
+        return books;
+    }
+    public Collection<Member> getMembers() {
+        return members;
+    }
+    //We don't need 'setter' because we don't want to change the private variable
+    //Method to add a book to the library
     public void addBook(Book book) {
-        if (!books.contains(book))      books.add(book);
+        if (books.contains(book))   System.out.println("Book with id '" + book.getId() + "' is already exists!");
+        else if (book.getId() == null)   System.out.println("Book with id '" + book.getId() + "' does not exist!");
+        else books.add(book);
     }
+    //Method to add a member to the library
     public void addMember(Member member) {
-        if (!members.contains(member))      members.add(member);
+        if (members.contains(member))   System.out.println("Member with id '" + member.getMemberId() + "' is already exists!");
+        else if (member.getMemberId() == null)   System.out.println("Member with id '" + member.getMemberId() + "' does not exist!");
+        else members.add(member);
     }
-    public void borrowBook(Member member, Book book) {
-        if (members.contains(member)) {
-            member.borrowBook(book);
-            books.remove(book);
+    //Method to borrow many books for a member
+    public void borrowBook(Member member, Book... bookList) {
+        if (!members.contains(member))
+            System.out.println("#####Member with id '" + member.getMemberId() + "' don't added to the library");
+        for (Book book : bookList) {
+            if (!books.contains(book))
+                System.out.println("#####Book with id '" + book.getId() + "' don't added to the library");
+            else    member.borrowBook(book);
         }
-        else    System.out.println("#####Member is not in the library");
     }
-    public void returnBook(Member member, Book book) {
-        if (members.contains(member)) {
-            member.returnBook(book);
-            books.add(book);
+    //Method to return many books for a member
+    public void returnBook(Member member, Book... bookList) {
+        if (!members.contains(member))
+            System.out.println("#####Member with id '" + member.getMemberId() + "' don't added to the library");
+        for (Book book : bookList) {
+            if (!books.contains(book))
+                System.out.println("#####Book with id '" + book.getId() + "' don't added to the library");
+            else    member.returnBook(book);
         }
-        else    System.out.println("#####Member is not in the library");
     }
     //Create printMemberInfo method
     public void printLibraryInfo() {
-        System.out.println("-----Name: " + name);
-        System.out.println("-----Books of the Library: ");
+        System.out.println("----Name: " + name);
+        System.out.println("----Books of the Library: ");
         int numberIndex = 1;
         for (Book book : books) {
             System.out.println(">>>Book: '" + numberIndex++ + "' :");
             book.printBookInfo();
-        }
-        System.out.println("-----Members of the Library: ");
+        }if (books.isEmpty()) System.out.println("Don't exist any books in the library");
+        System.out.println("----Members of the Library: ");
         numberIndex = 1;
         for (Member member : members) {
             System.out.println(">>>Member: '" + numberIndex++ + "' :");
             member.printMemberInfo();
-        }
+        }if (members.isEmpty()) System.out.println("Don't exist any members in the library");
     }
 }
